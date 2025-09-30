94363b59f64a0143ce58afc73a5c6048463a57ed
Yug Shah <yugshah197@gmail.com>
2025-09-30 17:31:05 +0530

    30 septermer

---
+++ 04_Day_Strings/day-4myfile.py
@@ -0,0 +1,19 @@
+#1
+# a,b,c,d = 'Thirty', 'Days', 'Of', 'Python'
+
+# print('{} {} {} {}'.format(a,b,c,d))
+
+#2
+# e,f,g = 'Coding', 'For' , 'All'
+# print('{} {} {}'.format(e,f,g))
+
+#3 4 5 6 7 8 9
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company[0:6])
