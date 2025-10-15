862ae67e564297261945b70c26c1416f0828a105
Yug Shah <yugshah197@gmail.com>
2025-10-15 19:32:26 +0530

    15-10-25

---
+++ 05_Day_Lists/day-5myfile.py
@@ -0,0 +1,21 @@
+empty_list = list()
+
+#2
+list = ["item1","item2","item3","item4","item5"]
+
+#3
+print(len(list))
+
+#4
+print(list[0:5:2])
+
+#5
+mixed_data_types =["Yug Shah",18,5.8,False,"103 Goare apt,90 feet Road,Bhyander(w)"]
+
+#6
+companies = ['Facebook', 'Google', 'Microsoft', 'Apple','IBM', 'Oracle ','Amazon']
+
+#7
+print(companies)
+
+#8
