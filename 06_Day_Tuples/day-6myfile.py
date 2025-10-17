075dbfa0d9bef12f29b9d9f471b16515bf537b79
Yug Shah <yugshah197@gmail.com>
2025-10-17 18:20:37 +0530

    17-10-25

--- 06_Day_Tuples/day-6myfile.py
+++ 06_Day_Tuples/day-6myfile.py
@@ -16,3 +16,27 @@ name_mother = ('Meena')
 
 family_members = siblings + name_mother + name_father
 
+#level 2
+fruits = ("apple", "banana", "orange", "grape", "mango", "pear", "peach", "plum", "papaya", "watermelon")
+vegetables = ("carrot", "broccoli", "spinach", "potato", "onion", "bell pepper")
+
+food_stuff_tp = fruits + vegetables
+
+#print(food_stuff_tp)
+
+#3
+food_stuff_lt = list(food_stuff_tp)
+
+#print(food_stuff_lt)
+
+#4
+
+#5
+#print(food_stuff_tp[0:3])
+#print(food_stuff_tp[-3:])
+
+#6
+del food_stuff_tp
+
+#7
+
