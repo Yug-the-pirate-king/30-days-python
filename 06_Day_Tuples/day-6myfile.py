431e3154cce9e29b5ed822cadbd8a46ed1c9c566
Yug Shah <yugshah197@gmail.com>
2025-10-17 17:51:48 +0530

    17-10-25

---
+++ 06_Day_Tuples/day-6myfile.py
@@ -0,0 +1,18 @@
+#1
+empty_tuple = tuple()
+
+#2
+name_sister = ('Khushi')
+name_brother = ('Yug')
+#3
+siblings = name_sister+name_brother
+
+#4
+#1
+
+#5
+name_father = ('Vipul')
+name_mother = ('Meena')
+
+family_members = siblings + name_mother + name_father
+
