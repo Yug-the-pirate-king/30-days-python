7fa8f3d027146ea338c845761aca080e334a48fc
Yug Shah <yugshah197@gmail.com>
2025-11-19 19:18:31 +0530

    19-11-25

---
+++ 09_Day_Conditionals/day-9myfile.py
@@ -0,0 +1,29 @@
+
+#level 1
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are old enough to learn to drive.")
+# else:
+#     print("You need "+str(18-age)+" more years to learn to drive.")
+
+
+# age_2 = int(input("Enter your age:"))
+
+# if age_2-25==1:
+#     print("You are 1 year older than me.")
+# elif 25-age_2 == 1:
+#     print("You are 1 year younger then me.")
+# elif age_2 == 25:
+#     print("We have the same age.")
+# elif age_2>= 26:
+#     print("You are "+str(age_2-25)+" years older than me.")
+# else:
+#     print("You are "+str(25-age_2)+" years younger then me.")
+
+# a = int(input("Enter number one : "))
+# b = int(input("Enter number two : "))
+
+# print(str(a)+" is greater than "+str(b)) if a > b else print(str(b)+" is greater than "+str(a))
+
+#level 2
