a67e7b07327eb8d2acc2003a9dedf01a617fecf0
Yug Shah <yugshah197@gmail.com>
2025-11-20 18:34:00 +0530

    20-11-25

--- 09_Day_Conditionals/day-9myfile.py
+++ 09_Day_Conditionals/day-9myfile.py
@@ -27,3 +27,62 @@
 # print(str(a)+" is greater than "+str(b)) if a > b else print(str(b)+" is greater than "+str(a))
 
 #level 2
+# scores = int(input("Enter your scores :"))
+
+# if scores <=100 and scores >= 80 :
+#     print("A")
+# elif scores <= 79 and scores >= 70 :
+#     print("B")
+# elif scores <= 69 and scores >= 60 :
+#     print("C")
+# elif scores <= 59 and scores >= 50 :
+#     print("D")
+# elif scores <= 49 and scores >= 0 :
+#     print("F")
+
+# Program to check the season based on month
+
+# month = input("Enter the month: ").strip().capitalize()
+
+# if month in ["September", "October", "November"]:
+#     print("The season is Autumn.")
+# elif month in ["December", "January", "February"]:
+#     print("The season is Winter.")
+# elif month in ["March", "April", "May"]:
+#     print("The season is Spring.")
+# elif month in ["June", "July", "August"]:
+#     print("The season is Summer.")
+# else:
+#     print("Invalid month entered.")
+
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+
+# user_fruits = input("Enter a fruits : ")
+
+# if user_fruits in fruits:
+#     print("fruit does exist.")
+# else :
+#     fruits.append(user_fruits)
+#     print("fruit doesn't exist !, Added")
+#     print(fruits)
+
+#level3
+
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+
+if 'skills' in person:
+    middle_index = len(person['skills']) // 2
+    print(person['skills'][middle_index])
+else:
+    print("No Skill")
