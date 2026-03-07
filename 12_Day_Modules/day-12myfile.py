30290b4ede5b6512c25a6ee242d15a36ed38e251
Yug Shah <yugshah197@gmail.com>
2026-03-07 20:48:15 +0530

    daliy code

--- 12_Day_Modules/day-12myfile.py
+++ 12_Day_Modules/day-12myfile.py
@@ -1,10 +1,19 @@
 import random
 import string
 
-def random_user_id(length = 6):
+def random_user_id(length):
     char_pool = string.ascii_letters + string.digits
 
     random_id = ''.join(random.choices(char_pool,k = length))
 
     return random_id
-print(random_user_id())
+print(random_user_id(6))
+
+def new_random_user_id(length,i):
+    char_pool = string.ascii_letters + string.digits
+    for s in range(i):
+        random_id = ''.join(random.choices(char_pool,k = length))
+        print(random_id)
+
+new_random_user_id(16,3)
+
