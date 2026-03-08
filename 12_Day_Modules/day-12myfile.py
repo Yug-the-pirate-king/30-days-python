8843f61b7fe64ad5bb3ad29f84c27dbbbffd5a04
Yug Shah <yugshah197@gmail.com>
2026-03-08 19:49:00 +0530

    daliy code 08/03

--- 12_Day_Modules/day-12myfile.py
+++ 12_Day_Modules/day-12myfile.py
@@ -18,9 +18,17 @@ def new_random_user_id(length,i):
 new_random_user_id(16,3)
 
 def rgb():
+
     color_values = [str(random.randint(0, 255)) for _ in range(3)]
     
     return ','.join(color_values)
 
 print(rgb())
-print(rgb())
+
+def generate_colors(st,i):
+
+    color_values = [f'#{random.randint(0, 0xFFFFFF):06x}' for _ in range(i)]
+
+    return ','.join(color_values)
+
+print(generate_colors('hex',3))
